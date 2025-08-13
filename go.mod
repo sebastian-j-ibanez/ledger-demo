7bc4d349172dd5dc4711439b57695bcec28f9736
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-08-13 18:30:13 -0400

    feat: add demo, update ledger to v0.3.0

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sebastian-j-ibanez/ledger-demo
 
 go 1.24.4
+
+require github.com/sebastian-j-ibanez/ledger v0.3.0
--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/sebastian-j-ibanez/ledger"
+)
+
+func main() {
+	l := ledger.NewLedger()
+
+	println()
+	for i := range 3 {
+		msg := fmt.Sprintf("This is the %d entry in the ledger.", i)
+		fmt.Printf("Appending node %d\n", i+1)
+		err := l.AddNode([]byte(msg))
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	fmt.Println("\nHashes:")
+	for _, node := range l.GetNodes() {
+		fmt.Printf("%d\n", node.Hash)
+	}
+
+	valid, err := l.ValidateLedger()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if valid {
+		println("\nLedger contents are cryptographically valid.\n")
+	} else {
+		println("\nLedger contents are NOT cryptographically valid.\n")
+	}
 }
